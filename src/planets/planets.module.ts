ad847b07c1356907d1ea8ad5a35809b0897d94b7
Vyctor <dev.vyctor@gmail.com>
2021-06-21 15:37:21 -0300

    implementando metodo que retorna a quantidade de aparicoes do planeta em filmes

--- src/planets/planets.module.ts
+++ src/planets/planets.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { PlanetsService } from './planets.service';
 import { PlanetsController } from './planets.controller';
 import PlanetSchema from './models/planet.schema';
+import { SwapiModule } from '../swapi/swapi.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Planet', schema: PlanetSchema }]),
+    SwapiModule,
   ],
   providers: [PlanetsService],
   controllers: [PlanetsController],
--- src/planets/planets.service.ts
+++ src/planets/planets.service.ts
@@ -3,12 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreatePlanetDto } from './dtos/create-planet.dto';
 import Planet from './models/planet.interface';
+import { SwapiService } from '../swapi/swapi.service';
 
 @Injectable()
 export class PlanetsService {
   constructor(
     @InjectModel('Planet')
     private readonly planetModel: Model<Planet>,
+    private readonly swapiService: SwapiService,
   ) {}
 
   async createPlanet(createPlanetDto: CreatePlanetDto): Promise<Planet> {
@@ -20,7 +22,13 @@ export class PlanetsService {
       throw new BadRequestException('Planet already registered');
     }
 
-    const newPlanet = new this.planetModel(createPlanetDto);
+    const numberOfMovies =
+      await this.swapiService.getPlanetAppearanceInMoviesService(name);
+
+    const newPlanet = new this.planetModel({
+      ...createPlanetDto,
+      numberOfMovies,
+    });
 
     return await newPlanet.save();
   }
