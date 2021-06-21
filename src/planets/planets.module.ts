62f2dfbb9f83de867403c37f4bb01a091e5176c6
Vyctor <dev.vyctor@gmail.com>
2021-06-21 15:07:33 -0300

    importando mongoose no modulo de planeta

--- src/planets/planets.module.ts
+++ src/planets/planets.module.ts
@@ -1,9 +1,14 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { PlanetsService } from './planets.service';
 import { PlanetsController } from './planets.controller';
+import PlanetSchema from './models/planet.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Planet', schema: PlanetSchema }]),
+  ],
   providers: [PlanetsService],
-  controllers: [PlanetsController]
+  controllers: [PlanetsController],
 })
 export class PlanetsModule {}
