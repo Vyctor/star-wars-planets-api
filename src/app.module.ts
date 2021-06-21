a196b6bafccf00c0905fe1dfc98c5991befae8d0
Vyctor <dev.vyctor@gmail.com>
2021-06-21 14:49:39 -0300

    alterando link do mongodb atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { PlanetsModule } from './planets/planets.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin@cluster0.kbn5i.mongodb.net/smartranking?retryWrites=true&w=majority',
+      'mongodb+srv://admin:admin@cluster0.nwnlo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
       {
         useNewUrlParser: true,
         useCreateIndex: true,
