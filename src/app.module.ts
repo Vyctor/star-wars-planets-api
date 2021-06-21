0f2821538d6c4ff14142787f35f2c61d784d6c83
Vyctor <dev.vyctor@gmail.com>
2021-06-21 14:48:57 -0300

    importando mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlanetsModule } from './planets/planets.module';
 
 @Module({
-  imports: [PlanetsModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.kbn5i.mongodb.net/smartranking?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: true,
+      },
+    ),
+    ,
+    PlanetsModule,
+  ],
 })
 export class AppModule {}
