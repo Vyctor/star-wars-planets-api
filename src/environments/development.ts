5d485fd2da8e146de25ca75fec1ce4a78de0f0be
Vyctor <dev.vyctor@gmail.com>
2021-06-21 22:26:08 -0300

    movendo porta do servidor para variaveis de ambiente

--- src/environments/development.ts
+++ src/environments/development.ts
@@ -1,6 +1,7 @@
 const environment = {
   MONGO_DB_ATLAS_URL:
     'mongodb+srv://admin:admin@cluster0.nwnlo.mongodb.net/star-wars-planets?retryWrites=true&w=majority',
+  SERVER_PORT: 3333,
 };
 
 export default environment;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import environment from './environments/development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3333);
+  await app.listen(environment.SERVER_PORT);
 }
 bootstrap();
